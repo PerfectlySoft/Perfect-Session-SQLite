cdf7688a73bf296e9f2f0c2721a290c3316cb3a5
Kyle Jessup <kyle.jessup@treefrog.ca>
2019-03-26 13:28:12 -0400

    Updated package file

--- Package.swift
+++ Package.swift
@@ -1,11 +1,17 @@
+// swift-tools-version:4.2
 // Generated automatically by Perfect Assistant Application
 // Date: 2017-10-03 20:20:46 +0000
 import PackageDescription
 let package = Package(
 	name: "PerfectSessionSQLite",
-	targets: [],
+	products: [
+		.library(name: "PerfectSessionSQLite", targets: ["PerfectSessionSQLite"])
+	],
 	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", majorVersion: 3),
-		.Package(url: "https://github.com/SwiftORM/SQLite-StORM.git", majorVersion: 3),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", from: "3.0.0"),
+		.package(url: "https://github.com/SwiftORM/SQLite-StORM.git", from: "3.0.0"),
+	],
+	targets: [
+		.target(name: "PerfectSessionSQLite", dependencies: ["SQLiteStORM"])
 	]
 )
